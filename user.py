26ac210c3abe713285f92b2dcae34f5f00abfaae
mzambia70 <mzambia70@gmail.com>
2019-05-12 23:50:32 +0300

    added a pickle function to get the user info

--- user.py
+++ user.py
@@ -2,6 +2,11 @@ import random
 import pickle
 
 info = {}
+
+with open("pass-display.mz","br") as readfile:
+    info = pickle.load(readfile)
+
+
 s = "abcdefghiklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 len_password = int(input("enter number of letters in password:"))
 
@@ -13,9 +18,9 @@ if("yes" in answer):
     account_name = input("enter name of account: ")
     info[account_name] = password
     with open("pass-display.mz","bw") as filewrite:
-        pickle.dump(info,filewrite)
+        pickle.dump(info,filewrite, protocol=2)
 
-        
+
 
 else:
     print("ok")
