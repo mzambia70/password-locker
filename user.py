1e115e557ba1d07dc9b1b38adc636a6c7d4dbd87
mzambia70 <mzambia70@gmail.com>
2019-05-12 23:39:00 +0300

    imported pickle to convert object to string and dump it into file

--- user.py
+++ user.py
@@ -1,11 +1,21 @@
 import random
+import pickle
 
+info = {}
 s = "abcdefghiklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
 len_password = int(input("enter number of letters in password:"))
 
 password ="".join(random.sample(s,len_password))
 print(password)
 
-#answer =input("like to keep pasword: ")
-#if("yes" in answer):
- #   account_name = input("enter name of account: ")
+answer =input("like to keep pasword: ")
+if("yes" in answer):
+    account_name = input("enter name of account: ")
+    info[account_name] = password
+    with open("pass-display.mz","bw") as filewrite:
+        pickle.dump(info,filewrite)
+
+        
+
+else:
+    print("ok")
